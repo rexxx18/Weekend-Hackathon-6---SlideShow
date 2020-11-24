0b01a0d82f7061d91d9a9358556b6358914e3dd0
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-11-24 08:33:45 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,52 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const [val, setval] = useState(0);
+  const handleClickprenext = () => {
+    if (val < props.slides.length - 1) {
+      setval(val + 1);
+    }
+  };
+  const handleClickprev = () => {
+    if (val > 0) {
+      setval(val - 1);
+    }
+  };
+
+  const handleClickRestart = () => {
+    setval(0);
+  };
+  // console.log(props.slides[0].title)
   return (
-    <></>
-  )
-}
-
+    <>
+      <div>
+        <h1 data-testid="title">{props.slides[val].title}</h1>
+        <p data-testid="text">{props.slides[val].text}</p>
+      </div>
+      <button
+        data-testid="button-prev"
+        onClick={handleClickprev}
+        disabled={val === 0 ? true : false}
+      >
+        previous
+      </button>
+      <button
+        data-testid="button-restart"
+        onClick={handleClickRestart}
+        disabled={val > 0 ? false : true}
+      >
+        Restart
+      </button>
+      <button
+        data-testid="button-next"
+        onClick={handleClickprenext}
+        disabled={val === props.slides.length - 1 ? true : false}
+      >
+        next
+      </button>
+    </>
+  );
+};
 
 export default App;
